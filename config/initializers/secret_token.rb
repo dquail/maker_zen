eee8291d7fa07135e82041bbcf57516982a9ca4e
David Quail <quail.david@gmail.com>
2014-06-19 12:16:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakerZen::Application.config.secret_token = '40713f608df5ecfbff53c42c464fa2a2cf89a2e4028511564175dd9cb78f09776ca88bd2afe0c602ed302093fceb263e5c5157e4debfe9e5aba2e35d89f46a9d'
